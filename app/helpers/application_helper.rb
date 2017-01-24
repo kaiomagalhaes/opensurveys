720920c33778ab7599c7fae05064bf31ba55eb3c
Gilmar Filho <gaxf12@gmail.com>
2017-01-24 22:33:34 +0000

    Setting the survey title to the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 module ApplicationHelper
+  def set_title(survey_title = '')
+    default_title = "Open Surveys"
+    if survey_title.empty?
+      default_title
+    else
+      survey_title
+    end
+  end
 end
