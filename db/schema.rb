868575dcc661ecf024779881b0fb48b32844edb1
Gilmar Filho <gaxf12@gmail.com>
2017-01-24 22:36:22 +0000

    Updating Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117211433) do
+ActiveRecord::Schema.define(version: 20170124152810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,18 @@ ActiveRecord::Schema.define(version: 20170117211433) do
     t.index ["survey_id"], name: "index_rapidfire_questions_on_survey_id", using: :btree
   end
 
+  create_table "rapidfire_survey_settings", force: :cascade do |t|
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "title",      default: "Survey"
+  end
+
   create_table "rapidfire_surveys", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "survey_setting_id"
+    t.index ["survey_setting_id"], name: "index_rapidfire_surveys_on_survey_setting_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
